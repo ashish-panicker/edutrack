e59a7fc09a0578f7bb3518458ff35b1bbedf312d
ashish-panicker <ashish.s.panicker@proton.me>
2025-04-07 10:55:10 +0530

    Discovery service

--- enrollment-service/src/main/java/com/example/enrollment_service/clients/CourseService.java
+++ enrollment-service/src/main/java/com/example/enrollment_service/clients/CourseService.java
@@ -1,12 +1,10 @@
 package com.example.enrollment_service.clients;
 
-import com.example.enrollment_service.dto.ResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "courseClient", url = "http://localhost:8090/courses")
+@FeignClient(name = "COURSE-SERVICE")
 public interface CourseService {
 
     @GetMapping("/{id}")
